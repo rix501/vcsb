a74152b4f44f53ce013a443628fc1ec87928396e
Ricardo J. Vazquez <rix501@gmail.com>
2012-01-10 17:51:13 +0000

    making head way to make it a npm/web package

---
+++ data.js
@@ -0,0 +1,7 @@
+module.exports = (function() {
+    return {
+        "cool" : [ "Cool", "Fascinating", "Riveting", "Sweet", "Amazing", "Delightful", "Engrossing" ],
+        "story" : [ "story", "discourse", "anecdote", "fable", "argument", "saga", "passage"],
+        "bro" : [ "bro", "chum", "comrade", "brethren", "colleague", "yo", "homie"]
+    };
+})();
--- vcsb.js
+++ vcsb.js
@@ -7,28 +7,44 @@
         vcsb = exports;
     }
     else {
-        vcsb = root.Vcsb = {};
+        vcsb = root.vcsb = {};
     }
     
     var _ = root._;
     if (!_ && (typeof require !== 'undefined')) _ = require('underscore')._;
     
     var $ = root.jQuery || root.Zepto;
+    
+    if (typeof require !== 'undefined'){
+         vcsb.data = require('data');
+    }
+    else {
+        vcsb.data = $.ajax({
+            url: "./data.json"
+        }); 
+    }
 
     var randomInt = function(min, max){
         return Math.floor(Math.random() * (max - min + 1)) + min;
     };
 
+    vcsb.buildMsg = function(data, cb){
+        var num1 = randomInt(0, data.cool.length-1);
+        var num2 = randomInt(0, data.story.length-1);
+        var num3 = randomInt(0, data.bro.length-1);
+        
+        cb(data.cool[num1] + " " + data.story[num2] + ", " + data.bro[num3]);
+    };
+
     vcsb.getMsg = function(cb){
-        $.ajax({
-            url: "./parts.json",
-            success: function(data){
-                var num1 = randomInt(0, data.cool.length-1);
-                var num2 = randomInt(0, data.story.length-1);
-                var num3 = randomInt(0, data.bro.length-1);
-                cb(data.cool[num1] + " " + data.story[num2] + ", " + data.bro[num3]);
-            }
-        });    
+        if(typeof vcsb.data.done !== 'undefined'){
+            vcsb.data.done(function(data){
+                vcsb.buildMsg(data, cb);
+            });
+        }
+        else{
+            vcsb.buildMsg(vcsb.data, cb);
+        }
     };
 
 }).call(this);
