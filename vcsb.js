5a3b768c99271a0be83af343bdf8a83e1ff94e4b
Ricardo J. Vazquez <rix501@gmail.com>
2012-01-10 15:26:46 +0000

    restructured to better suit traditional/npm packaging

--- vcsb.js
+++ vcsb.js
@@ -1,18 +1,34 @@
-var randomInt = function(min, max){
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+(function(){
 
-var vcsb = function(data){
-    var num1 = randomInt(0, data.cool.length-1);
-    var num2 = randomInt(0, data.story.length-1);
-    var num3 = randomInt(0, data.bro.length-1);
+    var root = this;
     
-    $("#theStory").html(data.cool[num1] + " " + data.story[num2] + ", " + data.bro[num3]);
-};
+    var Vcsb;
+    if (typeof exports !== 'undefined') {
+        Vcsb = exports;
+    }
+    else {
+        Vcsb = root.Vcsb = {};
+    }
+    
+    var _ = root._;
+    if (!_ && (typeof require !== 'undefined')) _ = require('underscore')._;
+    
+    var $ = root.jQuery || root.Zepto;
 
-$(function() {
-    $.ajax({
-        url: "./parts.json",
-        success: vcsb
-    });
-});
+    var randomInt = function(min, max){
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+
+    Vcsb.getMsg = function(cb){
+        $.ajax({
+            url: "./parts.json",
+            success: function(data){
+                var num1 = randomInt(0, data.cool.length-1);
+                var num2 = randomInt(0, data.story.length-1);
+                var num3 = randomInt(0, data.bro.length-1);
+                cb(data.cool[num1] + " " + data.story[num2] + ", " + data.bro[num3]);
+            }
+        });    
+    };
+
+}).call(this);
